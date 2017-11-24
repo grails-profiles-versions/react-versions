c312fb9fe2d6b988978ab42a7756ab9bc3206ddb
sdelamo <sergio.delamo@softamo.com>
2017-11-24 17:55:21 +0100

    3.3.2

--- versions/client/src/AppNav.js
+++ versions/client/src/AppNav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar, NavDropdown, Nav, MenuItem } from 'react-bootstrap';
 
-import grailsLogo from './images/grails-cupsonly-logo-white.svg';
+import grailsLogo from './images/grails.svg';
 import 'whatwg-fetch';
 
 function AppNav({serverInfo, clientInfo}) {
@@ -9,12 +9,10 @@ function AppNav({serverInfo, clientInfo}) {
   const {environment, appprofile, appversion, grailsversion, reloadingagentenabled, artefacts, plugins} = serverInfo;
 
   return(
-    <Navbar style={{backgroundColor: '#4D8618', backgroundImage: 'none', borderRadius: 0}}>
+    <Navbar style={{backgroundColor: '#424649', backgroundImage: 'none', borderRadius: 0}}>
       <Navbar.Header>
         <Navbar.Brand>
-          <i className="fa grails-icon">
-            <img src={grailsLogo} alt="Grails" />
-          </i> Grails
+            <img src={grailsLogo} alt="Grails" width="80" height="80"/>
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
@@ -22,11 +20,11 @@ function AppNav({serverInfo, clientInfo}) {
         <Nav pullRight>
           <NavDropdown eventKey="4" title="Application Status " id="app-status">
             <MenuItem eventKey="4.1">Environment: {environment}</MenuItem>
-            <MenuItem eventKey="4.2">Grails profile: {appprofile}</MenuItem>
+            <MenuItem eventKey="4.2">Grails profile: {appprofile ? appprofile.replace('org.grails.profiles:', '') : null}</MenuItem>
             <MenuItem eventKey="4.2">Grails version: {grailsversion}</MenuItem>
-            <MenuItem eventKey="4.3">React version: {clientInfo.react.replace('^', '')}</MenuItem>
+            <MenuItem eventKey="4.3">React version: {clientInfo ? clientInfo.react.replace('^', '') : null}</MenuItem>
             <MenuItem eventKey="4.3">Server version: {appversion}</MenuItem>
-            <MenuItem eventKey="4.3">Client version: {clientInfo.version}</MenuItem>
+            <MenuItem eventKey="4.3">Client version: {clientInfo ? clientInfo.version : null}</MenuItem>
             <MenuItem divider />
             <MenuItem eventKey="4.4">Reloading active: {reloadingagentenabled ? 'true' : 'false'}</MenuItem>
           </NavDropdown>
